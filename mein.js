90e4b10b822aeca541c534618fe88d6bdf724404
ACER <baxtiyor.gazimboyev@gmail.com>
2024-10-14 14:31:04 +0300

    js

---
+++ mein.js
@@ -0,0 +1,77 @@
+
+
+// //  birinchi vazifa boshlanishi 
+// let ism = prompt("ismingizni kiritin");
+// let familya = prompt("familyangizni kiritin");
+// let yoshi = prompt("yoshingizni  kiritin");
+// let tugilgankuni = prompt("tugilgan sanangiz kiritin");
+// let yashashJoyi = prompt("yashash joyini kritin kiritin");
+// let uyingiz = prompt("ul manzilini kiriting kiritin");
+// let telefon = prompt("telefon raqamini kiritin");
+// let tezdaAloqagachi = prompt("tezda aloqaga chiqamiz kiritin");
+// let telegramrasmiysayitmizgaOtishingizmumkin =prompt("ozodbeko")
+// let instagramimiz =prompt("ozodbeko")
+// console.log(`
+//     sizni ismimgiz: ${ism},
+//     Sizning familyangiz: ${familya},
+//     Sizning yoshingiz: ${yoshi},
+//     Sizning yashash joyingiz :${yoshi},
+//     tug'ilgan kun iningiz :${tugilgankuni}
+//     istiqomat qilayot gan joyingia :${yashashJoyi},
+//     uy manzilingiz :${uyingiz},
+//     Sizning telefon raqamingiz: ${telefon},
+//     tez orada aloqaga chiqish :${tezdaAloqagachi},
+//     telegiram rasmi sayitimiz :${telegramrasmiysayitmizgaOtishingizmumkin},
+//     instagiram mimiz :${instagramimiz},
+//     `)
+//     alert(`
+//         sizni ismimgiz: ${ism},
+//         Sizning familyangiz: ${familya},
+//         Sizning yoshingiz: ${yoshi},
+//         Sizning yashash joyingiz :${yoshi},
+//         tug'ilgan kun iningiz :${tugilgankuni}
+//         istiqomat qilayot gan joyingia :${yashashJoyi},
+//         uy manzilingiz :${uyingiz},
+//         Sizning telefon raqamingiz: ${telefon},
+//         tez orada aloqaga chiqish :${tezdaAloqagachi},
+//         telegiram rasmi sayitimiz :${telegramrasmiysayitmizgaOtishingizmumkin},
+//         instagiram mimiz :${instagramimiz},
+//         `);
+
+//         //   birinchi vazifa tugadi👆
+// ikinchi vazifa
+// let olma = +prompt("olmani narhi");
+// let shaftoli = +prompt("shaftolini nar hi");
+// let uzum = +prompt("uzumni nar hi");
+// let olcha = +prompt("olichani narhi");
+// let hurmo = +prompt("hurmoni narhi");
+
+// alert(
+//     `sizning o'rtacha qil gan bozorin giz : ${(olma + shaftoli + uzum + olcha + hurmo)
+//     }`
+// );
+
+// console.log(
+//     `sizning o'rtacha qil gan bozorin giz : ${(olma + shaftoli + uzum + olcha + hurmo)
+//     }`
+// );
+// end ikkinchi vazifa👆
+// uchinchi vazifa
+// let math = +prompt("Matematikadan olgan balingizini kiriting");
+// let english = +prompt("Ingliz tilidan olgan balingizini kiriting");
+// let rus = +prompt("Rus tilidan olgan balingizini kiriting");
+// let history = +prompt("Tarixan olgan balingizini kiriting");
+// let fizika = +prompt("Fizikidan olgan balingizini kiriting");
+
+// alert(
+//   `Sizning umumiy o'rtacha balingiz: ${
+//     (math + english + rus + history + fizika) / 5
+//   }`
+// );
+
+// console.log(
+//   `Sizning umumiy o'rtacha balingiz: ${
+//     (math + english + rus + history + fizika) / 5
+//   }`
+// );
+// end uchunchi vazifa👆
