57bcb46f527076b8d60df7a12a24675838afece4
ACER <baxtiyor.gazimboyev@gmail.com>
2024-10-14 19:47:33 +0300

    js

--- mein.js
+++ mein.js
@@ -1,44 +1,44 @@
 
 
-// //  birinchi vazifa boshlanishi 
-// let ism = prompt("ismingizni kiritin");
-// let familya = prompt("familyangizni kiritin");
-// let yoshi = prompt("yoshingizni  kiritin");
-// let tugilgankuni = prompt("tugilgan sanangiz kiritin");
-// let yashashJoyi = prompt("yashash joyini kritin kiritin");
-// let uyingiz = prompt("ul manzilini kiriting kiritin");
-// let telefon = prompt("telefon raqamini kiritin");
-// let tezdaAloqagachi = prompt("tezda aloqaga chiqamiz kiritin");
-// let telegramrasmiysayitmizgaOtishingizmumkin =prompt("ozodbeko")
-// let instagramimiz =prompt("ozodbeko")
-// console.log(`
-//     sizni ismimgiz: ${ism},
-//     Sizning familyangiz: ${familya},
-//     Sizning yoshingiz: ${yoshi},
-//     Sizning yashash joyingiz :${yoshi},
-//     tug'ilgan kun iningiz :${tugilgankuni}
-//     istiqomat qilayot gan joyingia :${yashashJoyi},
-//     uy manzilingiz :${uyingiz},
-//     Sizning telefon raqamingiz: ${telefon},
-//     tez orada aloqaga chiqish :${tezdaAloqagachi},
-//     telegiram rasmi sayitimiz :${telegramrasmiysayitmizgaOtishingizmumkin},
-//     instagiram mimiz :${instagramimiz},
-//     `)
-//     alert(`
-//         sizni ismimgiz: ${ism},
-//         Sizning familyangiz: ${familya},
-//         Sizning yoshingiz: ${yoshi},
-//         Sizning yashash joyingiz :${yoshi},
-//         tug'ilgan kun iningiz :${tugilgankuni}
-//         istiqomat qilayot gan joyingia :${yashashJoyi},
-//         uy manzilingiz :${uyingiz},
-//         Sizning telefon raqamingiz: ${telefon},
-//         tez orada aloqaga chiqish :${tezdaAloqagachi},
-//         telegiram rasmi sayitimiz :${telegramrasmiysayitmizgaOtishingizmumkin},
-//         instagiram mimiz :${instagramimiz},
-//         `);
+//  birinchi vazifa boshlanishi 
+let ism = prompt("ismingizni kiritin");
+let familya = prompt("familyangizni kiritin");
+let yoshi = prompt("yoshingizni  kiritin");
+let tugilgankuni = prompt("tugilgan sanangiz kiritin");
+let yashashJoyi = prompt("yashash joyini kritin kiritin");
+let uyingiz = prompt("ul manzilini kiriting kiritin");
+let telefon = prompt("telefon raqamini kiritin");
+let tezdaAloqagachi = prompt("tezda aloqaga chiqamiz kiritin");
+let telegramrasmiysayitmizgaOtishingizmumkin =prompt("ozodbeko")
+let instagramimiz =prompt("ozodbeko")
+console.log(`
+    sizni ismimgiz: ${ism},
+    Sizning familyangiz: ${familya},
+    Sizning yoshingiz: ${yoshi},
+    Sizning yashash joyingiz :${yoshi},
+    tug'ilgan kun iningiz :${tugilgankuni}
+    istiqomat qilayot gan joyingia :${yashashJoyi},
+    uy manzilingiz :${uyingiz},
+    Sizning telefon raqamingiz: ${telefon},
+    tez orada aloqaga chiqish :${tezdaAloqagachi},
+    telegiram rasmi sayitimiz :${telegramrasmiysayitmizgaOtishingizmumkin},
+    instagiram mimiz :${instagramimiz},
+    `)
+    alert(`
+        sizni ismimgiz: ${ism},
+        Sizning familyangiz: ${familya},
+        Sizning yoshingiz: ${yoshi},
+        Sizning yashash joyingiz :${yoshi},
+        tug'ilgan kun iningiz :${tugilgankuni}
+        istiqomat qilayot gan joyingia :${yashashJoyi},
+        uy manzilingiz :${uyingiz},
+        Sizning telefon raqamingiz: ${telefon},
+        tez orada aloqaga chiqish :${tezdaAloqagachi},
+        telegiram rasmi sayitimiz :${telegramrasmiysayitmizgaOtishingizmumkin},
+        instagiram mimiz :${instagramimiz},
+        `);
 
-//         //   birinchi vazifa tugadi👆
+        //   birinchi vazifa tugadi👆
 // ikinchi vazifa
 // let olma = +prompt("olmani narhi");
 // let shaftoli = +prompt("shaftolini nar hi");
